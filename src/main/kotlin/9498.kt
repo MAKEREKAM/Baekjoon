65d28948514b618538a654d7ead819a144f4af95
MAKEREKAM <kimyounghyun123456@gmail.com>
2023-10-11 20:04:52 +0900

    9498

---
+++ src/main/kotlin/9498.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+    if (a >= 90) println("A")
+    else if (a >= 80) println("B")
+    else if (a >= 70) println("C")
+    else if (a >= 60) println("D")
+    else println("F")
+}

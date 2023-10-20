f93831a23f6264213341aec3e12f850e13d9f982
MAKEREKAM <kimyounghyun123456@gmail.com>
2023-10-21 00:51:54 +0900

    2338

---
+++ src/main/kotlin/2338.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextBigInteger()
+    val b = sc.nextBigInteger()
+    println(a + b)
+    println(a - b)
+    println(a * b)
+}

faeeb96ed888b0a4ad86a0e2ee3c8d5183ad9162
MAKEREKAM <kimyounghyun123456@gmail.com>
2023-10-21 17:25:08 +0900

    17256

---
+++ src/main/kotlin/17256.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+    val c = sc.nextInt()
+    val d = sc.nextInt()
+    val e = sc.nextInt()
+    val f = sc.nextInt()
+    println("${d - c} ${e / b} ${f - a}")
+}

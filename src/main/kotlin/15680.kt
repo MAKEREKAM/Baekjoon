c0d0d6ae6df03d33e77eef4f7446507084c1bde4
MAKEREKAM <kimyounghyun123456@gmail.com>
2023-10-21 15:11:49 +0900

    15680

---
+++ src/main/kotlin/15680.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+    if (a == 0) println("YONSEI")
+    else println("Leading the Way to the Future")
+}

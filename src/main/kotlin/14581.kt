0ef5913a4898ccfaa0d75088b5fe0d1d96a1caeb
MAKEREKAM <kimyounghyun123456@gmail.com>
2023-10-21 13:20:47 +0900

    14581

---
+++ src/main/kotlin/14581.kt
@@ -0,0 +1,9 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val a = sc.next()
+    println(":fan::fan::fan:")
+    println(":fan::$a::fan:")
+    println(":fan::fan::fan:")
+}

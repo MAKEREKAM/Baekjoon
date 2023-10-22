3003349c724caf94d90c2b9e6c92c18f38f490f6
MAKEREKAM <kimyounghyun123456@gmail.com>
2023-10-22 14:44:21 +0900

    1038

---
+++ src/main/kotlin/1038.kt
@@ -0,0 +1,24 @@
+import java.util.Scanner
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+    if (a > 1022) {
+        println(-1)
+        return
+    }
+    val b : ArrayList<Long> = ArrayList()
+    for (i in 1..1023) {
+        var c = 0L
+        var d = i
+        for (j in 9 downTo 0) {
+            if (d % 2 == 1) c = c * 10 + j
+            d /= 2
+        }
+        b.add(c)
+    }
+
+    b.sort()
+
+    println(b[a])
+}
